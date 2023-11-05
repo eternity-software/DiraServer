eb6581aeebc2595764bdaf005ef0647c0c2a5a00
DanilaKisikov <kisikov03@mail.ru>
2023-11-05 20:35:29 +0300

    Add new field to Attachment.java

--- src/main/java/ru/etysoft/dira/requests/entities/Attachment.java
+++ src/main/java/ru/etysoft/dira/requests/entities/Attachment.java
@@ -5,6 +5,7 @@ public class Attachment {
     private String fileUrl;
     private long fileCreatedTime;
     private String fileName;
+    private String realFileName = "";
     private long size;
     private AttachmentType attachmentType;
 
@@ -34,6 +35,14 @@ public class Attachment {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getRealFileName() {
+        return realFileName;
+    }
+
+    public void setRealFileName(String realFileName) {
+        this.realFileName = realFileName;
     }
 
     public long getSize() {
